4022e33389fdcf9ab3de45ee252f11ad03bcbff6
BillGu19 <wyg2@duke.edu>
2019-11-02 17:47:23 -0400

    first commit

---
+++ Glimpse/Glimpse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Glimpse
+//
+//  Created by Tanvi Santhosh on 11/2/19.
+//  Copyright © 2019 Tanvi Santhosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
